a28bbfc0c037e82e4b68344c2dde5554d70a764d
Erhan Berber <erhanberber@usa.net>
2018-11-24 22:39:18 -0600

    161804251 - Added ups zone to the institutions active admin listing

--- app/admin/institutions.rb
+++ app/admin/institutions.rb
@@ -2,4 +2,11 @@ ActiveAdmin.register Institution do
   menu false
   permit_params :name, :code, :zip_code
   actions :all, :except => [:edit, :update, :destroy]
+  
+  index do
+    column :name
+    column :code
+    column :zip_code
+    column :ups_zone
+  end
 end
