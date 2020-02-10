b2e3137ff32c7285d2af234c7eb9d61744509b7b
Baowei Wei <baowei@upenn.edu>
2020-02-10 14:20:18 -0500

    Revert "Update active_admin.rb"
    This reverts commit 0ccdf2fd8d0f79a0bdd5b7a67aa3e55ec1ea212b.

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -138,7 +138,7 @@ ActiveAdmin.setup do |config|
   #
   # Enable and disable Batch Actions
   #
-  config.batch_actions = false
+  config.batch_actions = true
 
   # == Controller Filters
   #
@@ -196,7 +196,7 @@ ActiveAdmin.setup do |config|
   #   config.register_stylesheet 'my_stylesheet.css'
   #
   # You can provide an options hash for more control, which is passed along to stylesheet_link_tag():
-     config.register_stylesheet 'my_print_stylesheet.css', media: :print
+  #   config.register_stylesheet 'my_print_stylesheet.css', media: :print
   #
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
@@ -290,7 +290,7 @@ ActiveAdmin.setup do |config|
   # config.footer = 'my custom footer text'
 
   config.view_factory.footer = Footer
-=begin
+
   config.namespace :admin do |admin|
 
     admin.build_menu do |menu|
@@ -331,8 +331,7 @@ ActiveAdmin.setup do |config|
       end
     end
   end
-=end
-  
+
   # == Sorting
   #
   # By default ActiveAdmin::OrderClause is used for sorting logic
