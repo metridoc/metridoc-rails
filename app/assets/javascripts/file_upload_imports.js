eb67e2874dfc396394d394622013ae4026e7ea63
Jordan Owens <jkowens@gmail.com>
2020-04-15 12:37:01 -0400

    Refactor file upload/import progress indicators

--- app/assets/javascripts/file_upload_imports.js
+++ app/assets/javascripts/file_upload_imports.js
@@ -21,4 +21,42 @@ $(function() {
       enctype: 'multipart/form-data'
     });
   });
+
+  if (userOnFileUploadImportsPage()) {
+    if ($("div[_file_upload_import_id]").length) {
+      refreshProgressBar();
+      setInterval(function(){ refreshProgressBar(); }, 1000);
+    }
+  };
 });
+
+function userOnFileUploadImportsPage() {
+  var pathName = window.location.pathname;
+  return pathName.indexOf('tools_file_upload_imports') >= 0;
+};
+
+function refreshProgressBar() {
+  var progressing = false;
+  var div = $("div[_file_upload_import_id]");
+  var id = div.attr("_file_upload_import_id");
+
+  $.getJSON( "/admin/tools_file_upload_imports/" + id + ".json", function( data ) {
+    var total_rows_to_process = data.total_rows_to_process;
+    var n_rows_processed = data.n_rows_processed;
+    var status = data.status;
+
+    if (total_rows_to_process) {
+      var completed_perc = parseInt((n_rows_processed / total_rows_to_process) * 100);
+      if (completed_perc < 100) progressing = true;
+      var html = "";
+      html += n_rows_processed + ' of ' + total_rows_to_process + ' rows processed';
+      html += '<div style="width:500px;border:1px solid black;height: 25px;" id=progress-bar >' +
+                '<div style="height:100%;width: ' + completed_perc + '%;background-color:lightblue;text-align:center;vertical-align:middle;font-weight:bold;" ></div>' +
+              '</div>';
+      div.html(html);
+      if (progressing && completed_perc >= 100) {
+        document.location.href = "/admin/tools_file_upload_imports/" + id;
+      }
+    }
+  });
+}
