56da5b5f965f36cff0c5ffad8185334295f4901e
Erhan Berber <erhanberber@usa.net>
2020-05-19 23:05:31 -0500

    [#172864742] - Added db migration to set penn users as super admins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_023343) do
+ActiveRecord::Schema.define(version: 2020_05_20_035952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
