addc155c0745070ba9a5efcfebd1e2892ea22c8f
Andrew Kimball <andrewnk@upenn.edu>
2020-07-13 12:08:39 -0400

    Add mailer from address

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['MAILER_USR'],
   :password => ENV['MAILER_PWD'],
+  :from => ENV['MAILER_DEFAULT_FROM'],
   :domain => ENV['MAILER_DOMAIN'],
   :address => ENV['MAILER_HOST'],
   :port => 587,
