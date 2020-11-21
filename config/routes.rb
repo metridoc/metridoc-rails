1c47711a7d9cae1827969434471325f28d0cd9bc
John Kiser <jkiser@upenn.edu>
2020-11-20 19:04:15 -0500

    added additional file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'report_gallery/pcc', :to => redirect('pennCountryCollaborators.html')
   get 'report_gallery/wpp', :to => redirect('worldPennPublished-2.html')
   get 'report_gallery/wwp', :to => redirect('wosWorldPublishers-1.html')
+  get 'report_gallery/psc', :to => redirect('pennStateCollaborators.html')
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
