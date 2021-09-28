f50b3f3485d9063999992e7ae23d7b70aab30a58
Karin Gilje <karin.e.gilje@gmail.com>
2021-09-28 15:33:07 -0400

    Pointing to vis-network.js.map.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts', 'd3_chord')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts')
 
 
 # Precompile additional assets.
