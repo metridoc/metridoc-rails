d1391d0afe65502b2354dc3d5ea91270c2257c63
Karin Gilje <karin.e.gilje@gmail.com>
2021-10-04 10:03:48 -0400

    Removing vis-network information from configuration, will be autoloaded from node-modules.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,6 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts', 'd3_chord')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts')
 
 
 # Precompile additional assets.
