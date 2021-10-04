3738f1322f2ddc450fa136883e9349d058a75dfc
Karin Gilje <karin.e.gilje@gmail.com>
2021-10-04 10:16:34 -0400

    Adding the d3-loom requirement (needs to be installed by yarn into node-modules).

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,10 +20,17 @@
 //= require rails-ujs
 //= require turbolinks
 
-//= require vis-network
+//= require vis-network/dist/vis-network
 
 //= require_tree .
 
 //= require d3
 //= require d3_chord/d3.layout.chord.sort
 //= require d3_chord/d3.stretched.chord
+
+//= require d3-array/dist/d3-array
+//= require d3-collection/dist/d3-collection
+//= require d3-color/dist/d3-color
+//= require d3-interpolate/dist/d3-interpolate
+//= require d3-loom/build/d3-loom
+//= require d3-path/dist/d3-path
