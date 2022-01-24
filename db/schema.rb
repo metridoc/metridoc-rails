b9afda9a8f65a4834995c158a87d3031d6af829f
John Kiser <jkiser@upenn.edu>
2022-01-24 11:02:23 -0500

    added field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_154923) do
+ActiveRecord::Schema.define(version: 2022_01_24_144420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -259,6 +259,7 @@ ActiveRecord::Schema.define(version: 2021_12_23_154923) do
     t.string "patron_question"
     t.text "session_description"
     t.text "notes"
+    t.boolean "upload_record", default: true
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
