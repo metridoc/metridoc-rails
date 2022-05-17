eeb66c6015a865e55bdf41c9db9868f84aaaadce
Karin Gilje <karin.e.gilje@gmail.com>
2022-05-17 14:31:06 -0400

    Adding a head page for the Alma Demographics area

---
+++ app/admin/upenn_alma.rb
@@ -0,0 +1,12 @@
+ActiveAdmin.register_page "Upenn::Alma" do
+  menu false
+
+  content title: "UPenn Alma Demographics" do
+    resource_collection = ActiveAdmin.application.namespaces[:admin].resources
+    resources = resource_collection.select { |resource| resource.respond_to? :resource_class }
+    resources = resources.select{|r| /^UpennAlma::/.match(r.resource_name.name) }
+    resources = resources.sort{|a,b| a.resource_name.human <=> b.resource_name.human }
+
+    render partial: 'index', locals: {resources: resources}
+  end
+end
