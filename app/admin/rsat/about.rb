d2491603cc847d7e68e7a3b27ecac1a404be6ab1
Karin Gilje <karin.e.gilje@gmail.com>
2022-11-08 20:22:47 -0500

    Remove some bad testing code.

--- app/admin/rsat/about.rb
+++ app/admin/rsat/about.rb
@@ -2,11 +2,6 @@ ActiveAdmin.register_page "About", namespace: :rsat do
   menu false
 
   content title: "RSAT" do
-
-    unless current_admin_user.authorized?
-      redirect_to :admin_root
-    end
-
     render partial: 'index'
   end
 end
