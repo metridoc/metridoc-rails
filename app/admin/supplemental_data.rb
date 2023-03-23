96622f34d8d0b0996574de0908e27e7063ea82ad
Karin Gilje <karin.e.gilje@gmail.com>
2023-03-23 17:26:13 -0400

    Adding the GeoData::CountryCode model.

--- app/admin/supplemental_data.rb
+++ app/admin/supplemental_data.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register_page "SupplementalData" do
   content title: proc{ I18n.t("active_admin.supplemental_data") } do
     resource_collection = ActiveAdmin.application.namespaces[:admin].resources
     resources = resource_collection.select { |resource| resource.respond_to? :resource_class }
-    resources = resources.select{|r| r.resource_name.name.in?(["Institution", "UpsZone", "GeoData::ZipCode"]) }
+    resources = resources.select{|r| r.resource_name.name.in?(["Institution", "UpsZone"]) }
     resources = resources.sort{|a,b| a.resource_name.human <=> b.resource_name.human }
 
     render partial: 'index', locals: {resources: resources}
---
+++ app/models/geo_data/country_code.rb
@@ -0,0 +1,2 @@
+class GeoData::CountryCode < GeoData::Base
+end
