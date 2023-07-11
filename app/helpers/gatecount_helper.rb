f48b6901f3534878806d753464c9fb110f8f8c2f
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-11 11:36:03 -0400

    Save Changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -2,18 +2,19 @@ module GatecountHelper
 
   def library_table(library,fiscal_year)
     if library="Furness"
-        model::GateCounts::CardSwipe.connection.select_all(
+      GateCount::CardSwipe.connection.select_all(
           "SELECT
-               WHERE GateCountsCardSwipe.door_name='FURNESS TURNSTILE_ *FUR'
-               AND GateCountsCardSwipe.swipe_date BETWEEN '#{fiscal_year-1}-07-0               1' AND #{fiscal_year}-06-30';").rows
+               WHERE door_name='FURNESS TURNSTILE_ *FUR'
+               AND swipe_date BETWEEN '#{fiscal_year-1}-07-01'
+               AND #{fiscal_year}-06-30';").rows
     elsif library="Biotech"
-         model::GateCounts::CardSwipe.connection.select_all(
+      GateCount::CardSwipe.connection.select_all(
           "SELECT
-               WHERE GateCountsCardSwipe.door_name='BIO LIBRARY TURNSTILE GATE_                 *JSN' AND GateCountsCardSwipe.swipe_date BETWEEN '#{fiscal_year-                1}-07-01' AND #{fiscal_year}-06-30';").rows
+               WHERE door_name='BIO LIBRARY TURNSTILE GATE_*JSN' AND swipe_date                BETWEEN '#{fiscal_year-1}-07-01' AND #{fiscal_year}-06-30';").rows
     else
-         model::GateCounts::CardSwipe.connection.select_all(
+      GateCount::CardSwipe.connection.select_all(
           "SELECT
-               WHERE (GateCountsCardSwipe.door_name='VAN PELT LIBRARY TURN1_ *VP                L' OR GateCountsCardSwipe.door_name='VAN PELT LIBRARY TURN2_ *VP                L' OR GateCountsCardSwipe.door_name='VAN PELT LIBRARY USC HANDIC                AP ENT VERIFY_ *VPL' OR GateCountsCardSwipe.door_name='VAN PELT                 LIBRARY ADA DOOR_ *VPL') AND GateCountsCardSwipe.swipe_date BETW                EEN '#{fiscal_year-1}-07-01' AND #{fiscal_year}-06-30';").rows      
+               WHERE (door_name='VAN PELT LIBRARY TURN1_ *VPL' door_name='VAN PE                LT LIBRARY TURN2_ *VPL' OR door_name='VAN PELT LIBRARY USC HANDI                CAP ENT VERIFY_ *VPL' OR door_name='VAN PELT LIBRARY ADA DOOR_ *                VPL') AND swipe_date BETWEEN '#{fiscal_year-1}-07-01' AND #{fisc                al_year}-06-30';").rows      
     
     #Will need to put options for:
     #1) Library
