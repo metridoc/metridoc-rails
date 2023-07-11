667463db61afaf2302f5ae48b9f2e7479e4b5e45
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-11 11:39:46 -0400

    Save Changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -6,15 +6,15 @@ module GatecountHelper
           "SELECT
                WHERE door_name='FURNESS TURNSTILE_ *FUR'
                AND swipe_date BETWEEN '#{fiscal_year-1}-07-01'
-               AND #{fiscal_year}-06-30';").rows
+               AND '#{fiscal_year}-06-30';").rows
     elsif library="Biotech"
       GateCount::CardSwipe.connection.select_all(
           "SELECT
-               WHERE door_name='BIO LIBRARY TURNSTILE GATE_*JSN' AND swipe_date                BETWEEN '#{fiscal_year-1}-07-01' AND #{fiscal_year}-06-30';").rows
+               WHERE door_name='BIO LIBRARY TURNSTILE GATE_*JSN' AND swipe_date                BETWEEN '#{fiscal_year-1}-07-01' AND '#{fiscal_year}-06-30';").rows
     else
       GateCount::CardSwipe.connection.select_all(
           "SELECT
-               WHERE (door_name='VAN PELT LIBRARY TURN1_ *VPL' door_name='VAN PE                LT LIBRARY TURN2_ *VPL' OR door_name='VAN PELT LIBRARY USC HANDI                CAP ENT VERIFY_ *VPL' OR door_name='VAN PELT LIBRARY ADA DOOR_ *                VPL') AND swipe_date BETWEEN '#{fiscal_year-1}-07-01' AND #{fisc                al_year}-06-30';").rows      
+               WHERE (door_name='VAN PELT LIBRARY TURN1_ *VPL' door_name='VAN PE                LT LIBRARY TURN2_ *VPL' OR door_name='VAN PELT LIBRARY USC HANDI                CAP ENT VERIFY_ *VPL' OR door_name='VAN PELT LIBRARY ADA DOOR_ *                VPL') AND swipe_date BETWEEN '#{fiscal_year-1}-07-01' AND '#{fisc                al_year}-06-30';").rows      
     
     #Will need to put options for:
     #1) Library
