df3dad62ba29e4b0a9de837e59dd75d2973b1edf
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-11 14:06:10 -0400

    save changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -1,30 +1,62 @@
 module GatecountHelper
 
-  def library_table(library,fiscal_year)
+#Library options are "Van Pelt", "Biotech", "Furness", "All"
+#Student type= "Grad Student" or "Undergraduate Student"
+  
+  def library_table(library,fiscal_year,student_type)
     if library="Furness"
       output_table=GateCount::CardSwipe.connection.select_all(
-          "SELECT * FROM gate_count_card_swipes
+          "SELECT COUNT (school) FROM gate_count_card_swipes
                WHERE door_name='FURNESS TURNSTILE_ *FUR'
+               AND user_group='#{student_type}'
                AND swipe_date BETWEEN '#{fiscal_year-1}-07-01'
-               AND '#{fiscal_year}-06-30';")
+               AND '#{fiscal_year}-06-30'
+               GROUP BY school;")
     elsif library="Biotech"
       output_table=GateCount::CardSwipe.connection.select_all(
-          "SELECT * FROM gate_count_card_swipes
-               WHERE door_name='BIO LIBRARY TURNSTILE GATE_*JSN' AND swipe_date                BETWEEN '#{fiscal_year-1}-07-01' AND '#{fiscal_year}-06-30';")
+          "SELECT COUNT (school) FROM gate_count_card_swipes
+               WHERE door_name='BIO LIBRARY TURNSTILE GATE_*JSN'
+               AND user_group='#{student_type}'
+               AND swipe_date BETWEEN '#{fiscal_year-1}-07-01'
+               AND '#{fiscal_year}-06-30'
+               GROUP BY school;")
+    elsif library="Van Pelt"
+      output_table=GateCount::CardSwipe.connection.select_all(
+          "SELECT COUNT (school) FROM  gate_count_card_swipes
+               WHERE (door_name='VAN PELT LIBRARY TURN1_ *VPL'
+               OR door_name='VAN PELT LIBRARY TURN2_ *VPL'
+               OR door_name='VAN PELT LIBRARY USC HANDICAP ENT VERIFY_ *VPL'
+               OR door_name='VAN PELT LIBRARY ADA DOOR_ *VPL')
+               AND user_group='#{student_type}'
+               AND swipe_date BETWEEN '#{fiscal_year-1}-07-01'
+               AND '#{fiscal_year}-06-30'
+               GROUP BY school;") 
     else
       output_table=GateCount::CardSwipe.connection.select_all(
-          "SELECT * FROM  gate_count_card_swipes
-               WHERE (door_name='VAN PELT LIBRARY TURN1_ *VPL' door_name='VAN PE                LT LIBRARY TURN2_ *VPL' OR door_name='VAN PELT LIBRARY USC HANDI                CAP ENT VERIFY_ *VPL' OR door_name='VAN PELT LIBRARY ADA DOOR_ *                VPL') AND swipe_date BETWEEN '#{fiscal_year-1}-07-01' AND '#{fisc                al_year}-06-30';")    
-
+          "SELECT COUNT (school) FROM  gate_count_card_swipes
+               WHERE (door_name='VAN PELT LIBRARY TURN1_ *VPL'
+               OR door_name='VAN PELT LIBRARY TURN2_ *VPL'
+               OR door_name='VAN PELT LIBRARY USC HANDICAP ENT VERIFY_ *VPL'
+               OR door_name='VAN PELT LIBRARY ADA DOOR_ *VPL'
+               OR door_name='FURNESS TURNSTILE_ *FUR'
+               OR door_name='BIO LIBRARY TURNSTILE GATE_*JSN')
+               AND user_group='#{student_type}'
+               AND swipe_date BETWEEN '#{fiscal_year-1}-07-01'
+               AND '#{fiscal_year}-06-30'
+               GROUP BY school;")
+      
     #Will need to put options for:
-    #1) Library
-    #2) Financial Year
-    #3) School
-    #4)Gate counts Total or Percentage total
+    #1) Gate counts total and for unique count
 
     end
+
+    #Get the full gate counts:
+    all_counts=[0,0,0,0,0,0,0,0,0,0,0,0]
     
-    return output_table
+    #output_table.each do |row|
+      #row[]
+    
+    return output_table.to_a, all_counts
 
   end
 
