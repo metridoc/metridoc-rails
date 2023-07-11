563a515f727640451cccfcd4bf6fba5216b4d83d
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-11 12:05:48 -0400

    Save Changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -2,20 +2,20 @@ module GatecountHelper
 
   def library_table(library,fiscal_year)
     if library="Furness"
-      GateCount::CardSwipe.connection.select_all(
+      output_table=GateCount::CardSwipe.connection.select_all(
           "SELECT * FROM gate_count_card_swipes
                WHERE door_name='FURNESS TURNSTILE_ *FUR'
                AND swipe_date BETWEEN '#{fiscal_year-1}-07-01'
-               AND '#{fiscal_year}-06-30';").rows
+               AND '#{fiscal_year}-06-30';")
     elsif library="Biotech"
-      GateCount::CardSwipe.connection.select_all(
+      output_table=GateCount::CardSwipe.connection.select_all(
           "SELECT * FROM gate_count_card_swipes
-               WHERE door_name='BIO LIBRARY TURNSTILE GATE_*JSN' AND swipe_date                BETWEEN '#{fiscal_year-1}-07-01' AND '#{fiscal_year}-06-30';").rows
+               WHERE door_name='BIO LIBRARY TURNSTILE GATE_*JSN' AND swipe_date                BETWEEN '#{fiscal_year-1}-07-01' AND '#{fiscal_year}-06-30';")
     else
-      GateCount::CardSwipe.connection.select_all(
+      output_table=GateCount::CardSwipe.connection.select_all(
           "SELECT * FROM  gate_count_card_swipes
-               WHERE (door_name='VAN PELT LIBRARY TURN1_ *VPL' door_name='VAN PE                LT LIBRARY TURN2_ *VPL' OR door_name='VAN PELT LIBRARY USC HANDI                CAP ENT VERIFY_ *VPL' OR door_name='VAN PELT LIBRARY ADA DOOR_ *                VPL') AND swipe_date BETWEEN '#{fiscal_year-1}-07-01' AND '#{fisc                al_year}-06-30';").rows      
-    
+               WHERE (door_name='VAN PELT LIBRARY TURN1_ *VPL' door_name='VAN PE                LT LIBRARY TURN2_ *VPL' OR door_name='VAN PELT LIBRARY USC HANDI                CAP ENT VERIFY_ *VPL' OR door_name='VAN PELT LIBRARY ADA DOOR_ *                VPL') AND swipe_date BETWEEN '#{fiscal_year-1}-07-01' AND '#{fisc                al_year}-06-30';")    
+
     #Will need to put options for:
     #1) Library
     #2) Financial Year
@@ -24,6 +24,8 @@ module GatecountHelper
 
     end
     
+    return output table
+
   end
 
 end  
