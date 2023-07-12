1a9bf1d11094bcdf1bee4422aac13babe645df3d
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-12 10:53:55 -0400

    Save Changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -24,7 +24,7 @@ module GatecountHelper
            user_group='Grad Student' OR user_group='Undergraduate Student'
            AND door_name IN ('VAN PELT LIBRARY ADA DOOR_ *VPL', 'VAN PELT LIBRARY TURN1_ *VPL', 'VAN PELT LIBRARY TURN2_ *VPL', 'VAN PELT LIBRARY USC HANDICAP ENT VERIFY_ *VPL', 'FURNESS TURNSTILE_ *FUR', 'BIO LIBRARY TURNSTILE GATE_ *JSN')         GROUP BY 1, 2, 3, 4;")
       
-    return output_table.where(school:'College of Arts & Sciences')
+    puts output_table.where(school:'College of Arts & Sciences')
 
   end
 
