84b52a7b022e3fb9f0289ea75898989810e27b10
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-11 20:09:37 -0400

    Commitment is sexy!

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -24,6 +24,9 @@ module GatecountHelper
            user_group='Grad Student' OR user_group='Undergraduate Student'
            AND door_name IN ('VAN PELT LIBRARY ADA DOOR_ *VPL', 'VAN PELT LIBRARY TURN1_ *VPL', 'VAN PELT LIBRARY TURN2_ *VPL', 'VAN PELT LIBRARY USC HANDICAP ENT VERIFY_ *VPL', 'FURNESS TURNSTILE_ *FUR', 'BIO LIBRARY TURNSTILE GATE_ *JSN')         GROUP BY 1, 2, 3, 4;")
 
+
+    pop_stats=output_table
+      
     puts pop_stats.where(school: "College of Arts & Sciences")
     
     return pop_stats
