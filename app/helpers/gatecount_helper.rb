f2c6e23af6f4e141fc75d7593a27d005b76eed54
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-13 15:29:12 -0400

    Save Changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -81,7 +81,7 @@ module GatecountHelper
 
     percent_index=(0..percents.length-1).to_a
 
-    percent_index.each {|i| percents_array << schools[i] => percents[i]}
+    percent_index.each {|i| percents_array[schools[i]] => percents[i]}
     
     return percents_array
   end
