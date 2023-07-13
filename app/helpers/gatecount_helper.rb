ed3f4ea267806d8ffd12efbe49e03d550968fea3
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-13 14:01:12 -0400

    Save Changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -53,7 +53,8 @@ module GatecountHelper
       gen_values=gen_values.delete_if{|h| h["library"] == "Van Pelt" || h["library"] == "Biotech"}
    elif library="Van Pelt"
       gen_values=gen_values.delete_if{|h| h["library"] == "Furness" || h["library"] == "Biotech"}
-    end
+   return gen_values
+   end
     
   end
 
