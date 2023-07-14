1ec4586e4210be3f57d57f73f28dc2f295e065af
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-14 14:31:06 -0400

    Save Changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -66,7 +66,7 @@ module GatecountHelper
            COUNT(DISTINCT card_num) AS num_people
          FROM gate_count_card_swipes 
            WHERE school=='College of Arts & Sciences'
-              door_name IN ('VAN PELT LIBRARY ADA DOOR_ *VPL', 'VAN PELT LIBRARY TURN1_ *VPL', 'VAN PELT LIBRARY TURN2_ *VPL', 'VAN PELT LIBRARY USC HANDICAP ENT VERIFY_ *VPL', 'FURNESS TURNSTILE_ *FUR', 'BIO LIBRARY TURNSTILE GATE_ *JSN')
+              AND door_name IN ('VAN PELT LIBRARY ADA DOOR_ *VPL', 'VAN PELT LIBRARY TURN1_ *VPL', 'VAN PELT LIBRARY TURN2_ *VPL', 'VAN PELT LIBRARY USC HANDICAP ENT VERIFY_ *VPL', 'FURNESS TURNSTILE_ *FUR', 'BIO LIBRARY TURNSTILE GATE_ *JSN')
          GROUP BY 1,2,3,4;")
 
       return output_table.to_a
