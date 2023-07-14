eb1f0028268c335ccc0a9af90d070506dbde49bc
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-14 11:07:36 -0400

    Save Changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -20,13 +20,32 @@ module GatecountHelper
            COUNT(card_num) AS num_swipes, 
            COUNT(DISTINCT card_num) AS num_people  
          FROM gate_count_card_swipes 
-         WHERE
-           user_group='Grad Student' OR user_group='Undergraduate Student'
            AND door_name IN ('VAN PELT LIBRARY ADA DOOR_ *VPL', 'VAN PELT LIBRARY TURN1_ *VPL', 'VAN PELT LIBRARY TURN2_ *VPL', 'VAN PELT LIBRARY USC HANDICAP ENT VERIFY_ *VPL', 'FURNESS TURNSTILE_ *FUR', 'BIO LIBRARY TURNSTILE GATE_ *JSN')         GROUP BY 1, 2, 3, 4;")
 
       return output_table.to_a
   end
 
+  def time_table
+      output_table=GateCount::CardSwipe.connection.select_all(
+        "SELECT
+           CASE
+             WHEN door_name LIKE 'VAN PELT%'
+               THEN 'Van Pelt'
+             WHEN door_name LIKE 'FURNESS%'
+               THEN 'Furness'
+             WHEN door_name LIKE 'BIO%'
+               THEN 'Biotech'
+           END AS library,
+           DATE_PART('year', swipe_date + INTERVAL '6 month') AS fiscal_year,
+           EXTRACT(month from swipe_date) AS month,
+           COUNT(card_num) AS num_swipes, 
+           COUNT(DISTINCT card_num) AS num_people  
+         FROM gate_count_card_swipes 
+           AND door_name IN ('VAN PELT LIBRARY ADA DOOR_ *VPL', 'VAN PELT LIBRARY TURN1_ *VPL', 'VAN PELT LIBRARY TURN2_ *VPL', 'VAN PELT LIBRARY USC HANDICAP ENT VERIFY_ *VPL', 'FURNESS TURNSTILE_ *FUR', 'BIO LIBRARY TURNSTILE GATE_ *JSN')         GROUP BY 1, 2, 3")
+
+      return output_table.to_a
+  end
+  
   def enrollment_table #(user,fiscal_year)
     pop_table=Upenn::Enrollment.connection.select_all(
       "SELECT
@@ -71,7 +90,8 @@ module GatecountHelper
   end
 
   def calc_percents(input_table,type,user_group)
-    copy_table=input_table.select{|h| h["user_group"] == user_group}
+    if user_group != "All"
+       copy_table=input_table.select{|h| h["user_group"] == user_group}
     if type=="Counts"
      num_swipes=copy_table.pluck("num_swipes")
      all_swipes=num_swipes.sum 
