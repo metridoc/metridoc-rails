39df117c51883e7d091eccba79f5322d850bf6c9
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-18 16:27:27 -0400

    Save Changes

--- app/admin/population_penetration.rb
+++ app/admin/population_penetration.rb
@@ -1,14 +1,14 @@
 ActiveAdmin.register_page "Population & Penetration" do
 
   breadcrumb do
-    [link_to('Admin',admin_root_path), link_to('Populations & Penetration',admin_gatecount_path)]
+    [link_to('Admin',admin_root_path), link_to('Gate Counts',admin_gatecount_path)]
   end
 
   #Don't add to the top menu
   menu false
 
   #Title for the page
-  content title: "Gate Counts Overview" do
+  content title: "Populations & Penetration" do
     render partial: 'admin/gatecount/population'
   end
 
