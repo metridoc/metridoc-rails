082ae212485c154892eb57090f9140d5ee1f78b5
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-18 16:17:47 -0400

    Save Changes

---
+++ app/admin/gate_counts_overview.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register_page "Gate Counts Overview" do
+
+  breadcrumb do
+    [link_to('Admin',admin_root_path), link_to('Gate Counts',admin_gatecount_path)]
+  end
+
+  #Don't add to the top menu
+  menu false
+
+  #Title for the page
+  content title: "Gate Counts Overview" do
+    render partial: 'admin/gatecount/overview'
+  end
+
+  #Restrict the page view to the correct users:
+  controller do
+    private
+    def authorize_access!
+      authorize! :read, "GateCount"
+    end
+  end
+
+end
