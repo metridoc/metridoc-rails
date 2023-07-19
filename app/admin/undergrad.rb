45899566621c413e8eee928537434a7a33909ac0
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-19 12:57:09 -0400

    Save Changes

---
+++ app/admin/undergrad.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register_page "Undergraduate Statistics" do
+
+  breadcrumb do
+    [link_to('Admin',admin_root_path), link_to('Gate Counts',admin_gatecount_path),'Population',admin_population_path]
+  end
+
+  #Don't add to the top menu
+  menu false
+
+  #Title for the page
+  content title: "Undergraduate Statistics" do
+    render partial: 'admin/gatecount/undergrad_stats'
+  end
+
+  #Restrict the page view to the correct users:
+  controller do
+    private
+    def authorize_access!
+      authorize! :read, "GateCount"
+    end
+  end
+
+end
