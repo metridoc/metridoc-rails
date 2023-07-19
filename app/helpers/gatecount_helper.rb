a7ccab21a89c6a6c0fb157c6962098633e89bf2a
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-19 17:06:50 -0400

    Save Changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -197,7 +197,6 @@ module GatecountHelper
                 fiscal_data["#{year_range[y]}"] = fiscal_year_people
              end
          end
-         puts fiscal_data
          return fiscal_data
       end
                                                                      
@@ -343,6 +342,7 @@ module GatecountHelper
          
       end
 
+      puts all_data 
       return all_data
   end
 end
