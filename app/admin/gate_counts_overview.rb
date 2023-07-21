de3e72089f65f8517aa038cd32918d0b1e0ee952
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-21 10:38:59 -0400

    Save Changes!

--- app/admin/gate_counts_overview.rb
+++ app/admin/gate_counts_overview.rb
@@ -7,6 +7,12 @@ ActiveAdmin.register_page "Gate Counts Overview" do
   #Don't add to the top menu
   menu false
 
+   #Action needed to define variables for frequency plots
+  age_action :frequency, method: :get do
+    @input_school = params[:school]
+
+  end
+  
   #Title for the page
   content title: "Gate Counts Overview" do
     render partial: 'admin/gatecount/overview'
