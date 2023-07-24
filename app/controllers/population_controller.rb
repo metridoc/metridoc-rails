367818b547081c9c57d361e646bd537a5bc00a2d
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-24 16:44:00 -0400

    TRY IT!

--- app/controllers/population_controller.rb
+++ app/controllers/population_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   def create
     @input_school=params[:school]
     @input_library=params[:libary]
+    puts @input_school
+    puts @input_library
   end
   
 end
