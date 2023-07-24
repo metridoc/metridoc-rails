28018ae4049f1af6049c2790beb65401b551d5fe
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-24 11:43:45 -0400

    Lets test it!

--- app/assets/javascripts/population_penetration.js
+++ app/assets/javascripts/population_penetration.js
@@ -1,6 +1,7 @@
 $(admin_gatecount_path).on("ajax:success", function(event, data, status, xhr) {
     var input_school=data.school;
-    var input_library=data.library}
+    var input_library=data.library;
+    alert("Response is => " + input_school + input_library)}
   ).on("ajax:error", function(event, xhr, status, error) {
     var input_school="College of Arts and Sciences";
     var input_library="Van Pelt"});
