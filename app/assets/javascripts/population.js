7b274ebb46a2882f688369db8af85a3a32ba651d
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-24 15:46:02 -0400

    TEST IT!

--- app/assets/javascripts/population.js
+++ app/assets/javascripts/population.js
@@ -1,4 +1,4 @@
-$("admin_population_penetration_path").on("ajax:complete",'admin_population_penetration_path', function(event, data, status, xhr) {
+$(document).on("ajax:complete",'admin_population_penetration_path', function(event, data, status, xhr) {
     var input_school=data.school;
     var input_library=data.library;
     alert("Response is => " + input_school + input_library);
