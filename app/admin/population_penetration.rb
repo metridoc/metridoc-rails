1748bedccf9c4f67227f0d28a23f5d45f8ca3b18
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-24 16:18:47 -0400

    TRY IT!

--- app/admin/population_penetration.rb
+++ app/admin/population_penetration.rb
@@ -8,21 +8,12 @@ ActiveAdmin.register_page "Population & Penetration" do
   menu false
 
   #Action needed to define variables for frequency plots
-  page_action :population, method: :get do
+  page_action :population, method: :post do
     @input_school = params[:school]
     @input_library = params[:libary]
   end
 
   def index
-    ("admin_population_penetration_path").on("ajax:success",'admin_population_penetration_path', function(event, data, status, xhr) {
-    var input_school=data.school
-    var input_library=data.library
-    alert("Response is => " + input_school + input_library)
-    return input_school
-    return input_library}
-  ).on("ajax:error", function(event, xhr, status, error) {
-    var input_school="College of Arts and Sciences"
-    var input_library="Van Pelt"})
   end
     
   #Title for the page
--- app/assets/javascripts/population.js
+++ app/assets/javascripts/population.js
@@ -1,4 +1,4 @@
-$(population).on("ajax:complete",'admin_population_penetration_path', function(event, data, status, xhr) {
+$(document).on("ajax:complete",'admin_population_penetration_path', function(event, data, status, xhr) {
     var input_school=data.school;
     var input_library=data.library;
     alert("Response is => " + input_school + input_library);
