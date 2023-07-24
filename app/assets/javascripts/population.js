c113ab90810a7725ea9b89f1dd51cdc276a284d3
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-24 16:14:33 -0400

    TEST IT!

--- app/assets/javascripts/population.js
+++ app/assets/javascripts/population.js
@@ -1,18 +1,12 @@
-\\$(document).on("ajax:complete",'admin_population_penetration_path', function(e\\vent, data, status, xhr) {
-\\    var input_school=data.school;
-\\    var input_library=data.library;
-\\    alert("Response is => " + input_school + input_library);
-\\    return input_school;
-\\    return input_library};
-\\  ).on("ajax:error", function(event, xhr, status, error) {
-\\    var input_school="College of Arts and Sciences";
-\\    var input_library="Van Pelt"});
-
-$(document).ready ->
-    $(document).on("ajax:success", (e, data, status, xhr) ->
-    $(document).append xhr.responseText
-  ).on "ajax:error", (e, xhr, status, error) ->
-    $(document).append "<p>ERROR</p>"
+$(population).on("ajax:complete",'admin_population_penetration_path', function(event, data, status, xhr) {
+    var input_school=data.school;
+    var input_library=data.library;
+    alert("Response is => " + input_school + input_library);
+    return input_school;
+    return input_library};
+  ).on("ajax:error", function(event, xhr, status, error) {
+    var input_school="College of Arts and Sciences";
+    var input_library="Van Pelt"});
 
 var chart = Chartkick.charts["2"]
 
