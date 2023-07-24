5c1b89640f107311a1223ab7b06756c3de0ba4d1
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-24 15:36:22 -0400

    TEST IT!

--- app/admin/population_penetration.rb
+++ app/admin/population_penetration.rb
@@ -15,14 +15,14 @@ ActiveAdmin.register_page "Population & Penetration" do
 
   def index
     ("admin_population_penetration_path").on("ajax:success",'admin_population_penetration_path', function(event, data, status, xhr) {
-    var input_school=data.school;
-    var input_library=data.library;
-    alert("Response is => " + input_school + input_library);
-    return input_school;
-    return input_library};
+    var input_school=data.school
+    var input_library=data.library
+    alert("Response is => " + input_school + input_library)
+    return input_school
+    return input_library}
   ).on("ajax:error", function(event, xhr, status, error) {
-    var input_school="College of Arts and Sciences";
-    var input_library="Van Pelt"});
+    var input_school="College of Arts and Sciences"
+    var input_library="Van Pelt"})
   end
     
   #Title for the page
