2bab98daf738bde2cfe675a8b559ebfa2cf1be88
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-24 16:57:40 -0400

    TRY IT

--- app/admin/population_penetration.rb
+++ app/admin/population_penetration.rb
@@ -8,7 +8,7 @@ ActiveAdmin.register_page "Population & Penetration" do
   menu false
 
   #Action needed to define variables for frequency plots
-  page_action :population, method: :post do
+  page_action :population, method: :get do
     @input_school = params[:school]
     @input_library = params[:libary]
   end
