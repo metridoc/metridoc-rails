07c17701d97ee11e5b03aa6e168e4f0383cf2b9c
Karin Gilje <karin.e.gilje@gmail.com>
2023-07-25 13:25:51 -0400

    Adding more hidden columns to alma model.

--- app/models/alma/circulation.rb
+++ app/models/alma/circulation.rb
@@ -1,3 +1,9 @@
 class Alma::Circulation < Alma::Base
-  self.ignored_columns = [:first_name, :last_name, :penn_id_number, :preferred_email]
+  self.ignored_columns = [
+    :first_name,
+    :last_name,
+    :user_id,
+    :penn_id_number,
+    :preferred_email
+  ]
 end
