809bcfca0fcadc5b2d4d7b5c3842c3be2c7d36fa
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-25 12:46:12 -0400

    Save Changes!

--- app/assets/javascripts/population.js
+++ app/assets/javascripts/population.js
@@ -1,20 +1,18 @@
-//$(document).on("ajax:complete",'#random_id', function(event, data, status, xhr) {
-//    var input_school=data.school;
-//    var input_library=data.library;
-//    alert("Response is => " + input_school + input_library);
-//    return input_school;
-//    return input_library};
-//  ).on("ajax:error", function(event, xhr, status, error) {
-//    var input_school="College of Arts and Sciences";
-//    var input_library="Van Pelt"});
+$(document).on("ajax:complete",admin_population_penetration_path, function(event, data, status, xhr) {
+    var input_school=data.school;
+    var input_library=data.library;
+    alert("Response is => " + input_school + input_library);
+    return input_school;
+    return input_library};
+  ).on("ajax:error", function(event, xhr, status, error) {
+    var input_school="College of Arts and Sciences";
+    var input_library="Van Pelt"});
 
 //$.ajax({
 //        type: "GET",
 //        url: "admin_population_path",
 //        success: function(data){}
 //    }); 
-
-alert('Test was successful')
 
 var chart = Chartkick.charts["chart-id"]
 
