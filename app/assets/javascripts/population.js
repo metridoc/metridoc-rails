bb389edc32f6012d822109faddc83ec21638c1a9
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-25 13:14:36 -0400

    Save Changes!

--- app/assets/javascripts/population.js
+++ app/assets/javascripts/population.js
@@ -14,6 +14,8 @@ $(admin_population_penetration_path).on("ajax:success",admin_population_penetrat
 //        success: function(data){}
 //    }); 
 
+alert("Order Changed!")
+
 var chart = Chartkick.charts["0"]
 
 chart.redraw()
