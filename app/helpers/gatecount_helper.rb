6460d3df194b1809301a99080081d0e99fb25c3d
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-07-27 14:51:47 -0400

    Save Changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -151,7 +151,7 @@ module GatecountHelper
     if user_group == "Grad Student" || user_group == "Undergraduate Student"
       copy_table=input_table.select{|h| h["user_group"] == user_group}
     elsif user_group == "F/S"
-      copy_table=input_table.select{|h| h["user_group"].include? "Staff"}
+      copy_table=input_table.select{|h| h["user_group"].include? "Staff" || 'Faculty'}
     else
       copy_table=input_table
     end
