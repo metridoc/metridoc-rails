e523637dae6b5b982944e4b01e405fe7ac0a82ba
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-08-04 15:05:51 -0400

    Save Changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -351,6 +351,7 @@ module GatecountHelper
 
           week_table=week_table.select{|h| h["week"].to_i == week_range[i]}
 
+          puts "This is a test!"
           puts week_table
           
           single_user=week_table.pluck('single_user').sum
