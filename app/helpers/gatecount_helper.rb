5e26d465bf6de8d20ff3c6edb93ceb461f279538
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2023-08-11 12:13:37 -0400

    Save Changes

--- app/helpers/gatecount_helper.rb
+++ app/helpers/gatecount_helper.rb
@@ -332,7 +332,7 @@ module GatecountHelper
       
       enroll_names=['SAS','Wharton','Annenberg','Dental','Weitzman','Education','Engineering','Law','Perelman','Veterinary','Nursing','SP2']
 
-      puts school_index
+      puts enroll_names[school_index]
       puts num_users
       
       total_pop=enrollment_table("Total",fiscal_year)
