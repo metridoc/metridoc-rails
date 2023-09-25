84c40f7c7c6a9dd0ee445b1b6b39dff4d9a7573b
Karin Gilje <karin.e.gilje@gmail.com>
2023-09-25 11:07:37 -0400

    Adding IPEDS programs tables.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_12_134256) do
+ActiveRecord::Schema.define(version: 2023_09_24_182924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgstattuple"
@@ -1214,6 +1214,64 @@ ActiveRecord::Schema.define(version: 2023_09_12_134256) do
     t.string "format"
     t.string "imputationvar"
     t.text "var_title"
+  end
+
+  create_table "ipeds_program_schema", force: :cascade do |t|
+    t.string "varname"
+    t.string "data_type"
+    t.integer "fieldwidth"
+    t.string "format"
+    t.string "imputationvar"
+    t.text "var_title"
+    t.index ["varname"], name: "ipeds_program_schema_unique_id", unique: true
+  end
+
+  create_table "ipeds_programs", force: :cascade do |t|
+    t.integer "year"
+    t.integer "unitid"
+    t.text "cipcode"
+    t.integer "ptotal"
+    t.integer "ptotalde"
+    t.integer "ptotaldes"
+    t.integer "passoc"
+    t.integer "passocde"
+    t.integer "passocdes"
+    t.integer "pbachl"
+    t.integer "pbachlde"
+    t.integer "pbachldes"
+    t.integer "pmastr"
+    t.integer "pmastrde"
+    t.integer "pmastrdes"
+    t.integer "pdocrs"
+    t.integer "pdocrsde"
+    t.integer "pdocrsdes"
+    t.integer "pdocpp"
+    t.integer "pdocppde"
+    t.integer "pdocppdes"
+    t.integer "pdocot"
+    t.integer "pdocotde"
+    t.integer "pdocotdes"
+    t.integer "pcert1"
+    t.integer "pcert1_de"
+    t.integer "pcert1_a"
+    t.integer "pcert1_ade"
+    t.integer "pcert1_ades"
+    t.integer "pcert1_b"
+    t.integer "pcert1_bde"
+    t.integer "pcert1_bdes"
+    t.integer "pcert2"
+    t.integer "pcert2_de"
+    t.integer "pcert2_des"
+    t.integer "pcert4"
+    t.integer "pcert4_de"
+    t.integer "pcert4_des"
+    t.integer "ppbacc"
+    t.integer "ppbaccde"
+    t.integer "ppbaccdes"
+    t.integer "ppmast"
+    t.integer "ppmastde"
+    t.integer "ppmastdes"
+    t.index ["year", "unitid", "cipcode"], name: "ipeds_programs_unique_id", unique: true
   end
 
   create_table "ipeds_stem_cipcodes", force: :cascade do |t|
