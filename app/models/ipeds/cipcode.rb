3caaaa25ec8bd1c1b89e690fe8df58d2d1b3fae0
kgilje <karin.e.gilje@gmail.com>
2024-07-02 11:33:31 -0400

    Add on conflict update rules to ipeds models

--- app/models/ipeds/cipcode.rb
+++ app/models/ipeds/cipcode.rb
@@ -1,2 +1,8 @@
 class Ipeds::Cipcode < Ipeds::Base
+  
+  # Define rules for updating on conflict
+  def self.on_conflict_update
+    ["cip_code2010", "cip_code2020"]
+  end
+
 end
--- app/models/ipeds/completion.rb
+++ app/models/ipeds/completion.rb
@@ -1,2 +1,8 @@
 class Ipeds::Completion < Ipeds::Base
+  
+  # Define rules for updating on conflict
+  def self.on_conflict_update
+    ["year", "unitid", "cipcode", "majornum", "awlevel"]
+  end
+  
 end
