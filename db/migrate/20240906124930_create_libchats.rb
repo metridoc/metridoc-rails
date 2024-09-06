185312e94da6828257abef042e634888cf5ec411
abrivera887 <137329789+abrivera887@users.noreply.github.com>
2024-09-06 12:55:33 -0400

    Added Libchat Table Creation File

---
+++ db/migrate/20240906124930_create_libchats.rb
@@ -0,0 +1,33 @@
+class LibChats < ActiveRecord::Migration[7.1]
+  def change
+    create_table :libchats do |t|
+      t.integer :chat_id
+      t.string :name
+      t.string :contact_info
+      t.string :ip_address
+      t.string :browser
+      t.string :operating_system
+      t.string :user_agent
+      t.string :referrer
+      t.string :widget
+      t.string :department
+      t.string :answerer
+      t.datetime :timestamp
+      t.integer :wait_time
+      t.integer :duration
+      t.integer :rating
+      t.string :comment
+      t.string :user_field_1
+      t.string :user_field_2
+      t.string :user_field_3
+      t.string :initial_question
+      t.string :transfer_history
+      t.integer :message_count
+      t.string :internal_note
+      t.string :transcript
+      t.string :tags
+      t.integer :ticket_id
+      t.integer :ra_transaction_ids
+    end  
+  end
+end  
