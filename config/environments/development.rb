4cb4610d77e8ce1aaff4d8cd1f881aff68384764
Viet Nguyen <vietnguy@upenn.edu>
2024-09-18 15:51:27 -0400

    update docker compose etc

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,4 +82,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+   config.hosts << "metridoc-dev.library.upenn.edu"
 end
