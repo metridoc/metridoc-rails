1737add9043263b8f23cc299cd66a6c28cd72b92
Viet Nguyen <vietnguy@upenn.edu>
2025-03-04 15:12:46 -0500

    try removing molecule and local inventory tech debt

--- delayed-job-log-forwarder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Forwards the delayed_job logs to the Docker logs.
-set -e
-if [[ ! -e /home/app/webapp/log/delayed_job.log ]]; then
-    touch /home/app/webapp/log/delayed_job.log
-fi
-
-exec tail -F /home/app/webapp/log/delayed_job.log
