0f9cfec3aff4af91fe36860c7f558faa7170cbde
andrewnk <andrewnk@upenn.edu>
2025-05-15 15:52:19 -0400

    Allow local traffic

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,5 +83,9 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-   config.hosts << "metridoc-dev.library.upenn.edu"
+  config.hosts = [
+    "metridoc_app",
+    "localhost",
+    "metridoc-dev.library.upenn.edu"
+  ]
 end
