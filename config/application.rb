5fc8d0223281bd4a79cec06a595ca678af4cc5fb
andrewnk <andrewnk@upenn.edu>
2025-06-03 10:39:56 -0400

    Add class to read any docker secret

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require_relative '../utilities/docker_secrets'
+
 module MetridocsRails510
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
