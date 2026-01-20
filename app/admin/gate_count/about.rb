dc527c71ee8733c5663c61a993874d67f2432e84
kgilje <karin.e.gilje@gmail.com>
2026-01-20 10:48:19 -0500

    Bugfix to allow users to use the gate counts dashboard button

--- app/admin/gate_count/about.rb
+++ app/admin/gate_count/about.rb
@@ -4,7 +4,7 @@ namespace: :gate_count do
   menu false
 
   #Action needed to define variables for frequency plots
-  page_action :population, method: :post do
+  page_action :statistics, method: :post do
     redirect_url = "/gate_count/population_penetration?"
     redirect_url += "year=#{params[:year]}"
     redirect_to redirect_url
