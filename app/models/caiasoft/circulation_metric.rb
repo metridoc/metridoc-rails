4a94878b2d0aefd9756a8a6583944dc904fcdc80
kgilje <karin.e.gilje@gmail.com>
2026-02-11 19:35:48 -0500

    Add conflict resolution on unique keys in models.

--- app/models/caiasoft/circulation_metric.rb
+++ app/models/caiasoft/circulation_metric.rb
@@ -1,2 +1,10 @@
 class Caiasoft::CirculationMetric < Caiasoft::Base
+
+  def self.on_conflict_update
+    {
+      conflict_target: [:item_retrieved, :request_id, :job],
+      columns: self.column_names.map(&:to_sym) - [:id, :item_retrieved, :request_id, :job]
+    }
+  end
+
 end
