110b415be66f3a2b45adb72fe5a6978c5251da31
kgilje <karin.e.gilje@gmail.com>
2026-07-13 13:00:47 -0400

    Preparing for import anonymization

--- app/models/alma/circulation.rb
+++ app/models/alma/circulation.rb
@@ -1,9 +1,26 @@
 class Alma::Circulation < Alma::Base
-  self.ignored_columns = [
-    :first_name,
-    :last_name,
-    :user_id,
-    :penn_id_number,
-    :preferred_email
-  ]
+  include Pseudonymizable
+
+  before_save :anonymize_patrons
+
+  def carrel_user?
+    user_group == "Carrel"
+  end
+
+  def anonymize_patrons
+    id_str = penn_id_number.to_s
+
+    if penn_id_number.nil?
+      self.penn_id_number = "Unknown"
+    elsif carrel_user?
+      self.penn_id_number = id_str
+    else
+      # Use the Penn ID Number to generate a pseudonym
+      self.penn_id_number = generate_hmac_pseudonym(id_str)
+      
+      # Delete all non carrel first and last names
+      self.first_name = nil
+      self.last_name = nil
+    end
+  end
 end
